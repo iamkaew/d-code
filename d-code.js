9b366495c04f983a4409273752404094e6f19389
iamkaew <sirikayon.c@gmail.com>
2015-11-17 20:20:29 +0700

    try routing

--- d-code.js
+++ d-code.js
@@ -1,17 +1,57 @@
+players = new Mongo.Collection("players");
+
+Router.route('/', function () {
+  this.render('questioner');
+});
+
+Router.route('/player/:no', function () {
+  if(players.findOne({_id:this.params.no})) {
+    this.render('player');
+  }
+  else {
+    this.render('register');
+  }
+});
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  // Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
+  Template.questioner.helpers({
+    players: function() {
+      return players;
+    }
+  });
+  Template.questioner.events({
+    'click button': function() {
+      players = [];
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.player.helpers({
+    player: function () {
+      return players.findOne({_id:this.params.no}).name;
+    }
+  });
+
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
+  Template.register.events({
+    'submit form': function (event) {
+      var
+      player = event.target.player.value;
+
+      event.preventDefault();
+      players.insert({_id:this.params.no, name: player, score: 0});
     }
   });
 }
