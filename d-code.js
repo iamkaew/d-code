8207e1bd8b7698d23ae723a9884b2642f5cc3723
iamkaew <sirikayon.c@gmail.com>
2015-11-18 20:47:20 +0700

    almost done

--- d-code.js
+++ d-code.js
@@ -1,11 +1,13 @@
-players = new Mongo.Collection("players");
+players = new Mongo.Collection('players');
+questions = new Mongo.Collection('ques_set');
+code = 0;
 
 Router.route('/', function () {
   this.render('questioner');
 });
 
-Router.route('/player/:no', function () {
-  if(players.findOne({_id:this.params.no})) {
+Router.route('/player', function () {
+  if(Session.get('player')) {
     this.render('player');
   }
   else {
@@ -14,50 +16,104 @@ Router.route('/player/:no', function () {
 });
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  // Session.setDefault('counter', 0);
+  Session.setDefault('q_idx', Math.round(Math.random() * 3) + 1);
+  prepare = function() {
+    ques_set.forEach(function(set) {
+      set.sort(function(a, b) {
+        return Math.random() < 0.5 ? 1 : -1;
+      });
+    });
+  };
 
-  // Template.hello.helpers({
-  //   counter: function () {
-  //     return Session.get('counter');
-  //   }
-  // });
-  Template.questioner.helpers({
-    players: function() {
-      return players;
+  document.onkeydown = function (e) {
+    return (e.which || e.keyCode) != 116;
+  };
+
+  Tracker.autorun(function() {
+    if(players.find({}).count() == 0) {
+      Session.set('player', null);
     }
   });
+  
+  Template.questioner.helpers({
+    players: function() {
+      return players.find({}, {sort: {score: -1}});
+    },
+    question: function() {
+      // ques_set = questions.find({}).fetch();
+      // prepare();
+      // Session.set('question', ques_set[s_idx][q_idx++]);
+      Session.set('question', questions.findOne(Session.get('q_idx')));
+      code = Session.get('question').ans;
+      return Session.get('question').ask;
+    }
+  });
+
   Template.questioner.events({
-    'click button': function() {
-      players = [];
+    'click #reset': function() {
+      Meteor.call('resetPlayers')
+    },
+    'click #next': function() {
+      Session.set('q_idx', Math.round(Math.random() * 3) + 1);
     }
   });
 
   Template.player.helpers({
     player: function () {
-      return players.findOne({_id:this.params.no}).name;
+      if(players.find({}).count() == 0) {
+        Session.set('player', null);
+      }
+      return Session.get('player');
     }
   });
 
-  // Template.hello.events({
-  //   'click button': function () {
-  //     // increment the counter when button is clicked
-  //     Session.set('counter', Session.get('counter') + 1);
-  //   }
-  // });
+  Template.player.events({
+    'submit form': function (event) {
+      event.preventDefault();
+      Meteor.call('checkCode', Session.get('player'), event.target.ans.value);
+    }
+  });
+
   Template.register.events({
     'submit form': function (event) {
       var
-      player = event.target.player.value;
+      player = players.findOne({name: Session.get('player')}),
+      player_name = event.target.player.value,
+      _id;
 
       event.preventDefault();
-      players.insert({_id:this.params.no, name: player, score: 0});
+      if(player) {
+        Session.set('err', 'มีผู้เล่นชื่อนี้แล้ว');
+      }
+      else {
+        _id = players.insert({name: player_name, score: 0});
+        player = players.findOne(_id);
+        Session.set('player', player.name);
+      }
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    return Meteor.methods({
+      resetPlayers: function() {
+        return players.remove({});
+      },
+      checkCode: function(_name, _code) {
+        if(_code == '7735') {
+          players.update({name: _name}, {$inc: {score: 1}});
+        }
+      }
+    });
   });
 }
+
+/*
+db.ques_set.insert([
+  {_id: 1, ask: 'พระจันทร์ตั้งโด่เด่', ans: '3002'},
+  {_id: 2, ask: 'เปาบุ้นจิ้น', ans: '1200'},
+  {_id: 3, ask: 'บนฟ้าเป็ดร้องอู๊ด', ans: '2827'},
+  {_id: 4, ask: 'กลับหัวขายไม่เอาอังกฤษ', ans: '7735'}
+]);
+*/
