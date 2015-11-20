2aea0e6d15f402b910de0e7bb61b5beff447b8fe
iamkaew <sirikayon.c@gmail.com>
2015-11-20 19:54:09 +0700

    finish

--- d-code.js
+++ d-code.js
@@ -15,11 +15,10 @@ Router.route('/player', function () {
 });
 
 if(Meteor.isClient) {
+  // ส่วนควบคุมการแสดงคำถาม
   var
-  config = {
-    'max player': 3,
-    'resv size' : 10
-  },
+  max_player = 4,
+  resv_size = 10,
   resv_ids = [],
   showQuestion = function() {
     var
@@ -28,16 +27,16 @@ if(Meteor.isClient) {
     do {
       _id = Math.round(Math.random() * (questions.find({}).count() - 1)) + 1;
     } while(resv_ids.indexOf(_id) > -1);
-    if(resv_ids.length == config['resv size']) {
+    if(resv_ids.length == resv_size) {
       resv_ids.shift();
     }
     resv_ids.push(_id);
     question = questions.findOne(_id);
     Session.set('ask', question.ask);
+    Session.set('solved', false);
     Meteor.call('setSolution', question.ans);
   };
 
-  // ส่วนควบคุมการแสดงคำถาม
   Session.setDefault('wait', true);
   Template.questioner.helpers({
     wait: function() {
@@ -59,6 +58,7 @@ if(Meteor.isClient) {
       showQuestion();
     },
     'click #next': function() {
+      Session.set('solved', false);
       showQuestion();
     },
     'click #reset': function() {
@@ -75,11 +75,15 @@ if(Meteor.isClient) {
       }
       else if(players.findOne({name: Session.get('player')}).init) {
         Session.set('result', '');
+        Session.set('blink', '');
       }
       return Session.get('player');
     },
     result: function() {
       return Session.get('result');
+    },
+    blink: function() {
+      return Session.get('blink');
     }
   });
   Template.player.events({
@@ -87,7 +91,16 @@ if(Meteor.isClient) {
       event.preventDefault();
       Meteor.call('checkCode', Session.get('player'), event.target.ans.value, function(err, result) {
         event.target.ans.value = '';
-        Session.set('result', result);
+        if(result == 0) {
+          Session.set('result', 'ช้าไปนิส.. เค้าตอบไปแล้ว');
+        }
+        else if(result == 1) {
+          Session.set('result', 'ถูกต้องแล้วคร๊าบ');
+          Session.set('blink', 'blink');
+        }
+        else {
+          Session.set('result', 'ยังไม่ถูก ลองอีกที');
+        }
       });
     }
   });
@@ -108,7 +121,7 @@ if(Meteor.isClient) {
       _id;
 
       event.preventDefault();
-      if(players.find({}).count() == config['max player']) {
+      if(players.find({}).count() == max_player) {
         Session.set('error', 'ผู้เล่นครบแล้ว');
       }
       else if(players.findOne({name: player_name})) {
@@ -122,6 +135,9 @@ if(Meteor.isClient) {
       }
     }
   });
+  Template.register.rendered = function() {
+    this.find('input').focus();
+  }
 
   document.onkeydown = function (e) {
     return (e.which || e.keyCode) != 116;
@@ -140,70 +156,23 @@ if (Meteor.isServer) {
       },
       checkCode: function(_name, _code) {
         if(solved) {
-          return 'Solved';
+          return 0;
         }
         else if(_code == code) {
           solved = true;
           players.update({}, {$set: {status: '', init: false}});
           players.update({name: _name}, {$inc: {score: 1}, $set: {status: 'solver'}});
-          return 'Correct';
+          return 1;
         }
         else {
-          return 'Try again';
+          return 2;
         }
       },
       setSolution: function(_code) {
         code = _code;
         solved = false;
-        players.update({}, {$set: {init: true}});
+        players.update({}, {$set: {status: '', init: true}});
       }
     });
   });
 }
-
-/*
-db.questions.insert([
-  {_id: 1, ask: 'โสมฝรั่งตั้งโด่เด่', ans: '3002'},
-  {_id: 2, ask: 'เปาบุ้นจิ้น', ans: '1200'},
-  {_id: 3, ask: 'บนฟ้าเป็ดร้องอู๊ด', ans: '2827'},
-  {_id: 4, ask: 'กลับหัวขายอังกฤษ', ans: '7735'},
-  {_id: 5, ask: 'แก้วแรดสามฤดู', ans: '3000'},
-  {_id: 6, ask: 'ภัครมัยขำไข่ปลาหมึก', ans: '3508'},
-  {_id: 7, ask: 'ตุ๊กตุ๊กทั่วประเทศสู้ตาย', ans: '3772'},
-  {_id: 8, ask: 'เพื่อนมดชี้ทุกทิศชีวิตเหมียว', ans: '4189'},
-  {_id: 9, ask: 'บุเรงนองบอกแก่นัก แต่ยอดรักบอกยังแจ๋ว', ans: '1030'},
-  {_id: 10, ask: 'สไปเดอร์แมนคีบแขนทศกัณฐ์', ans: '8220'},
-  {_id: 11, ask: 'พวงมาลัยมหัศจรรย์', ans: '1007'},
-  {_id: 12, ask: 'องคุลีมาลโดนสนธิเดือด', ans: '1099'},
-  {_id: 13, ask: 'ปลามังกรแกะปู', ans: '3147'},
-  {_id: 14, ask: 'MISTERSHORTFOOT', ans: '9000'},
-  {_id: 15, ask: 'กระบือยานนาวาปลาสลิด', ans: '3102'},
-  {_id: 16, ask: 'เหนือท่วมใต้ฝุ่นตกแน่', ans: '1966'},
-  {_id: 17, ask: 'โทรศัพท์จากอุดรไปหาทักษิณ', ans: '2580'},
-  {_id: 18, ask: 'ผัวหนูปาหนี้', ans: '2559'},
-  {_id: 19, ask: 'สร้อยเสือเผ่น สยองยักษ์เร้นหลบหนี', ans: '1002'},
-  {_id: 20, ask: 'รถไฟไทยสามสีมีนายกฯ', ans: '5867'},
-  {_id: 21, ask: 'ชู้เมาฆ่าตีนแมว', ans: '3512'},
-  {_id: 22, ask: 'น้ำพริกปลายามาเย็นตาปาดังเบ', ans: '2543'},
-  {_id: 23, ask: 'ตี๋ใหญ่ขำไม่ได้ เสือไบก็ไม่ฮา', ans: '0000'},
-  {_id: 24, ask: 'พักนี้กี้ไม่อยู่', ans: '1000'},
-  {_id: 25, ask: 'ฉันเห็นฝรั่งตัวใหญ่ในกระจก', ans: '3321'},
-  {_id: 26, ask: 'ก่อนอี้ล้มเณรเปื้อน', ans: '9638'},
-  {_id: 27, ask: 'ประหยัดไฟกลางคืน', ans: '1245'},
-  {_id: 28, ask: 'แก๊งสามช่านั่งยองๆ', ans: '2400'},
-  {_id: 29, ask: 'อ่างอึ่งอ๋องอ๋องอึ่ง', ans: '1221'},
-  {_id: 30, ask: 'จุรีรักจา', ans: '1310'},
-  {_id: 31, ask: 'หินอ่อนแจ้งพระแก้ว', ans: '5101'},
-  {_id: 32, ask: 'อาตมาฉันมันมัน', ans: '1133'},
-  {_id: 33, ask: 'อีกท้ายนรสิงห์ ยศจริงพลายงาม', ans: '9000'},
-  {_id: 34, ask: 'มนุษย์หลังฉาก', ans: '9037'},
-  {_id: 35, ask: 'ลบห้าสอยกระทิงทิ้งสิบห้า', ans: '1512'},
-  {_id: 36, ask: 'กรมกสิกรตอนต้นเดือน', ans: '7181'},
-  {_id: 37, ask: 'กดเหนือดาราเลี้ยวขวาเมื่อสุด', ans: '7412'},
-  {_id: 38, ask: 'อาหารหน้าเห็ดหลังเป็ดนับปี', ans: '1210'},
-  {_id: 39, ask: 'ไฮโซหน้าเล็ก เขียนเช็คกลับหัว', ans: '0514'},
-  {_id: 40, ask: '007', ans: '2017'},
-  {_id: 41, ask: 'ความหลังในโหล', ans: '1162'},
-  {_id: 42, ask: 'ผู้ดีอ่านข่าวบนนาฬิกาไม่ออก', ans: '1296'}
-]);
-*/
